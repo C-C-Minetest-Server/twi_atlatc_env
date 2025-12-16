eeb65346ee3183e0ecbd7202b22cc0fdfbfb8a78
1F616EMO <root@1f616emo.xyz>
2025-12-16 18:43:07 +0800

    more ads

--- 1F616EMO/src/03-config-advertisements.lua
+++ 1F616EMO/src/03-config-advertisements.lua
@@ -17,10 +17,32 @@ F.WFC_mourning = {
 F.pis_advertisements = {
     -- Exactly 26 bytes lone, and two lines
 
+    --[[ TEMPOARAY ADS GOES HERE ]]--
+
+    -- TEMPORARY: Honor to Ho Wai-Ho
+    -- Fireman passed away in Tai Po Fire
+    -- Full-honor funeral: 2025-12-19
+    -- Scheduled removal: 2025-12-21
+    {
+        ">>  FULL HONOR TO LATE  <<",
+        ">>  FIREMAN  HO WAI-HO  <<"
+    },
+
+    -- TEMPORARY: Gathering idea for annual WFC fire mourning
+    -- Scheduled removal: 2026-01-01
+    {
+        ">> IDEAS ON WFC ANNUAL <<",
+        ">> MOURNING: /feedback <<"
+    },
+
+    --[[ RAILWAY SERVICE NOTICES ]]--
+
     -- Stay behind yellow line
     F.yellow_line_warning,
 
-    -- Should br copied to occupy 1/3 of the ads in annual mourning
+    --[[ OFFICIAL ADS ]]--
+
+    -- Should be copied to occupy 1/3 of the ads in annual mourning
     F.WFC_mourning,
 
     -- Join Discord server
@@ -33,5 +55,17 @@ F.pis_advertisements = {
     {
         ">>  HAVE SUGGESTIONS?   <<",
         ">>   USE /report CMD    <<"
+    },
+
+    -- Use newcomers plots within 14 days
+    {
+        ">>  USE NEWCOMERS PLOTS <<",
+        ">>    WITHIN 14 DAYS    <<"
+    },
+
+    -- /day
+    {
+        ">>   HATE NIGHTTIME?    <<",
+        ">>     USE /day CMD     <<"
     },
 }
