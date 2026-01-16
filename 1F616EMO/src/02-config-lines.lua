e435f555bc9384fa87cd0424bd7a7b956ed6b4b8
1F616EMO <root@1f616emo.xyz>
2026-01-16 22:16:57 +0800

    Change term display

--- 1F616EMO/src/02-config-lines.lua
+++ 1F616EMO/src/02-config-lines.lua
@@ -424,7 +424,7 @@ F.lines["S21"] = {
     code = "S21",
     name = "S-Bahn Line 21",
     W = "eEAN",
-    E = "M27-CFB",
+    E = "M27-SOL",
 
     show_complementary_station = true,
     adjacent_stations = construct_adjacent_stations({
